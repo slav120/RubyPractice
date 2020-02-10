736cfaa6bcc476132495626b0827bacd2b50b3f0
Slav <slalev57@me.com>
2020-02-09 20:46:33 -0500

    unless

--- else.rb
+++ else.rb
@@ -7,3 +7,12 @@ elsif 10 == 5
 else
   puts "10 is greater than 5, so I'm being printed!"
 end
+
+
+hungry = false
+
+unless hungry 
+  puts "I'm writing Ruby programs!"
+else 
+  puts "Time to eat!"
+end 
