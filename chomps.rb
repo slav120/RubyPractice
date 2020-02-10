a8d94f2ffe42d6eb6f51af20c603b3abf1403374
Slav <slalev57@me.com>
2020-02-09 20:30:53 -0500

    more chomps

--- chomps.rb
+++ chomps.rb
@@ -50,3 +50,16 @@ state = gets.chomp
 state.upcase!
 
 puts "Your name is #{first_name} #{last_name} and you're from #{city}, #{state}!"
+
+
+print "Integer please: "
+user_num = Integer(gets.chomp)
+
+if user_num < 0
+  puts "You picked a negative integer!"
+elsif user_num > 0
+  puts "You picked a positive integer!"
+else
+  puts "You picked zero!"
+end
+
