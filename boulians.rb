7576356f37bfbd4cf238c776778a3fcfd47dc07d
Slav <slalev57@me.com>
2020-02-22 21:39:11 -0500

    more booleans comparisons

--- boulians.rb
+++ boulians.rb
@@ -3,3 +3,14 @@
 ( 1 == 1 ) && ( 2 == 2 )  # true
 ( 1 == 2 ) || ( 2 == 2 ) # true
 !( false ) # true
+
+
+
+# test_1 should be true
+test_1 = (3 == 3) || (4 != 5)
+
+# test_2 = should be true
+test_2 = (5 > 3) && (7 < 10)
+
+# test_3 = should be false
+test_3 = (5 != 5)
